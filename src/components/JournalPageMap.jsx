17c1c5a4faa6f2d0cf34438a7a5cbfdc77856672
Soulweaver <soulweaver@hotmail.fi>
2018-06-17 02:20:45 +0300

    Make the map tab a little less WIP-y

--- src/components/JournalPageMap.jsx
+++ src/components/JournalPageMap.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { Alert } from "reactstrap";
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
 
 class JournalPageMap extends React.Component {
   render() {
-    return <div className="JournalPageMap">map</div>;
+    return (
+      <div className="JournalPageMap">
+        <Alert color="warning">
+          <FontAwesomeIcon icon="exclamation-triangle" /> This feature has not
+          been implemented yet, but it will be added in the future.
+        </Alert>
+      </div>
+    );
   }
 }
 
