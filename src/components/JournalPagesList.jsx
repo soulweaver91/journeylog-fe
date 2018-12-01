42bf6aab6355ec9eb9ea349eb73348cdb7bddece
Soulweaver <soulweaver@hotmail.fi>
2018-12-01 06:51:59 +0200

    Add photo counts to journal page list

--- src/components/JournalPagesList.jsx
+++ src/components/JournalPagesList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ListGroup, ListGroupItem } from "reactstrap";
 import { NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class JournalPagesList extends React.Component {
   render() {
@@ -16,7 +17,14 @@ class JournalPagesList extends React.Component {
           </ListGroupItem>
           {this.props.journey.journalPages.map((page) => (
             <ListGroupItem key={page.slug} tag={NavLink} to={page.route}>
-              {page.displayName}
+              <span className="JournalPagesList__page-name">
+                {page.displayName}
+              </span>
+              {page.photosCount > 0 && (
+                <span className="JournalPagesList__page-indicator">
+                  <FontAwesomeIcon icon="image" /> {page.photosCount}
+                </span>
+              )}
             </ListGroupItem>
           ))}
         </ListGroup>
