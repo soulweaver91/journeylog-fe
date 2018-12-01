eb450fa9d13a7f939f614fbdbe864b23c6264d91
Soulweaver <soulweaver@hotmail.fi>
2018-12-01 21:56:24 +0200

    Handle the journal page type

--- src/models/JournalPageBase.jsx
+++ src/models/JournalPageBase.jsx
@@ -1,19 +1,25 @@
 import { types, getParent } from "mobx-state-tree";
 
+export const JournalPageType = {
+  REGULAR: "REGULAR",
+  SPECIAL: "SPECIAL"
+};
+
 const JournalPageBase = types
   .model("JournalPageBase", {
     slug: types.string,
     dateStart: types.maybe(types.string),
     dateEnd: types.maybe(types.string),
     name: types.maybe(types.string),
-    photosCount: types.number
+    photosCount: types.number,
+    type: types.enumeration(Object.values(JournalPageType))
   })
   .views((self) => ({
     get route() {
       return `${getParent(self, 2).route}/${self.slug}`;
     },
     get displayName() {
-      if (!self.dateStart) {
+      if (!self.dateStart || self.type !== JournalPageType.REGULAR) {
         return self.name || "(unnamed page)";
       }
 
