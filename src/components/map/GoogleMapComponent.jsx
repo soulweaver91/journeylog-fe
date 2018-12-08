3cd05b1e4eb2b0bb108a97500feb9c3468a5fa74
Soulweaver <soulweaver@hotmail.fi>
2018-12-08 09:10:55 +0200

    Use latest Maps version
    Some issues indicated it wouldn't work, but seems to be working just fine.

--- src/components/map/GoogleMapComponent.jsx
+++ src/components/map/GoogleMapComponent.jsx
@@ -17,7 +17,7 @@ export const GoogleMapComponent = ({
   ...rest
 }) => (
   <InnerGoogleMapComponent
-    googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.33&key=${
+    googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&key=${
       process.env.REACT_APP_GOOGLE_MAPS_API_KEY
     }&libraries=geometry,drawing,places`}
     loadingElement={<div style={{ height: `100%` }} />}
